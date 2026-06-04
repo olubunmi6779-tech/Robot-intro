c73e8501406fc565ac24f50a31f33796c0e22fc5
olubunmi6779-tech <olubunmi6779@gmail.com>
2026-06-04 12:39:19 +0100

    Add files via upload

--- ac1.py
+++ ac1.py
@@ -1,6 +1,6 @@
-#  Introduce My Robot MAX
+#  Introduce My Robot TOM
 
-class Robotmax:
+class RobotTom:
     def __init__(self, name, model, purpose):
         self.name = name
         self.model = model
@@ -13,7 +13,30 @@ class Robotmax:
 
 
 # Creating an object
-my_robot = Robotmax(" Max", " 2.0", " to provide assitance")
+my_robot = RobotTom(" Tom", " 1.0", " to provide assitance")
 
 # Calling the method
 my_robot.introduce()
+
+#  Introduce My Robot Jerry
+
+class RobotJerry:
+    def __init__(self, name, model, purpose):
+        self.name = name
+        self.model = model
+        self.purpose = purpose
+
+    def introduce(self):
+        print("Hi there! My name is", self.name)
+        print("My model is", self.model)
+        print("My purpose is", self.purpose)
+
+
+# Creating an object
+my_robot = RobotJerry("Jerry", " 2.0", " to be a backup for Tom")
+
+# Calling the method
+my_robot.introduce()
+
+
+
